bcaee6282e27784f64dc619bcb5587bf0408dbe0
alpersahinoner <alpersahinoner@gmail.com>
2024-12-01 14:35:23 +0300

    First Commit (gitIgnore,ProjectMainClass,pom.xml)

---
+++ stremapi/src/main/java/com/streamapiegitimleri/StreamApiApplication.java
@@ -0,0 +1,12 @@
+package com.streamapiegitimleri;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiApplication {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+    }
+}
